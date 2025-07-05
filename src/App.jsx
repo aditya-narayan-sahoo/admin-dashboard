e28730a46e7a89c0fe793e8bdcac50860a9456b8
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2025-07-05 16:18:37 +0530

    Add routing logic and the pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import Sales from "./pages/Sales";
+import Users from "./pages/Users";
+import Orders from "./pages/Orders";
+import Overview from "./pages/Overview";
+import Products from "./pages/Products";
+import Settings from "./pages/Settings";
+import Analytics from "./pages/Analytics";
+
+import Sidebar from "./components/common/Sidebar";
+
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
   return (
-    <div className="text-3xl text-center underline font-serif">
-      Admin Dashboard
-    </div>
+    <main className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      <div className="fixed inset-0 z-0">
+        <section className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80">
+          <div className="absolute inset-0 backdrop-blur-sm" />
+        </section>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Overview />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/sales" element={<Sales />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/analytics" element={<Analytics />} />
+        </Routes>
+      </div>
+    </main>
   );
 };
 
