973e9b61c8d6e5a9de7cb6665207812262760b26
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2025-07-05 23:32:41 +0530

    Add the sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,20 @@ import { Route, Routes } from "react-router-dom";
 const App = () => {
   return (
     <main className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
-      <div className="fixed inset-0 z-0">
-        <section className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80">
-          <div className="absolute inset-0 backdrop-blur-sm" />
-        </section>
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Overview />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/sales" element={<Sales />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/analytics" element={<Analytics />} />
-        </Routes>
-      </div>
+      <section className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
+      </section>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Overview />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/sales" element={<Sales />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/analytics" element={<Analytics />} />
+      </Routes>
     </main>
   );
 };
