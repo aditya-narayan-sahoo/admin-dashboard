afeb343551412910d80dc98f85689d2f1e91493c
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2025-07-06 23:48:35 +0530

    Implenent the analytics components and page

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,5 +1,32 @@
-const Analytics = () => {
-  return <div>Analytics</div>;
-};
+import Header from "../components/common/Header";
 
-export default Analytics;
+import RevenueChart from "../components/analytics/RevenueChart";
+import OverviewCards from "../components/analytics/OverviewCards";
+import UserRetention from "../components/analytics/UserRetention";
+import AIPoweredInsights from "../components/analytics/AIPoweredInsights";
+import ChannelPerformance from "../components/analytics/ChannelPerformance";
+import ProductPerformance from "../components/analytics/ProductPerformance";
+import CustomerSegmentation from "../components/analytics/CustomerSegmentation";
+
+const AnalyticsPage = () => {
+  return (
+    <div className="flex-1 overflow-auto relative z-10 bg-gray-900">
+      <Header title={"Analytics Dashboard"} />
+
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        <OverviewCards />
+        <RevenueChart />
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
+          <ChannelPerformance />
+          <ProductPerformance />
+          <UserRetention />
+          <CustomerSegmentation />
+        </div>
+
+        <AIPoweredInsights />
+      </main>
+    </div>
+  );
+};
+export default AnalyticsPage;
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,22 @@
-const Settings = () => {
-  return <div>Settings</div>;
-};
+import Header from "../components/common/Header";
+import Profile from "../components/settings/Profile";
+import Security from "../components/settings/Security";
+import DangerZone from "../components/settings/DangerZone";
+import Notifications from "../components/settings/Notifications";
+import ConnectedAccounts from "../components/settings/ConnectedAccounts";
 
-export default Settings;
+const SettingsPage = () => {
+  return (
+    <div className="flex-1 overflow-auto relative z-10 bg-gray-900">
+      <Header title="Settings" />
+      <main className="max-w-4xl mx-auto py-6 px-4 lg:px-8">
+        <Profile />
+        <Notifications />
+        <Security />
+        <ConnectedAccounts />
+        <DangerZone />
+      </main>
+    </div>
+  );
+};
+export default SettingsPage;
