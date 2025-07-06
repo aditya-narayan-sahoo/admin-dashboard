d82d75df33bc2ceb6fec7d392e5eb05c675274a6
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2025-07-06 20:01:12 +0530

    Implenent the orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,59 @@
-const Orders = () => {
-  return <div>Orders</div>;
-};
+import { motion } from "framer-motion";
+import { CheckCircle, Clock, DollarSign, ShoppingBag } from "lucide-react";
 
-export default Orders;
+import { orderStats } from "../utils";
+import Header from "../components/common/Header";
+import StatCard from "../components/common/StatCard";
+import OrdersTable from "../components/orders/OrdersTable";
+import DailyOrders from "../components/orders/DailyOrders";
+import OrderDistribution from "../components/orders/OrderDistribution";
+
+const OrdersPage = () => {
+  return (
+    <div className="flex-1 relative z-10 overflow-auto">
+      <Header title={"Orders"} />
+
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard
+            name="Total Orders"
+            icon={ShoppingBag}
+            value={orderStats.totalOrders}
+            color="#6366F1"
+          />
+          <StatCard
+            name="Pending Orders"
+            icon={Clock}
+            value={orderStats.pendingOrders}
+            color="#F59E0B"
+          />
+          <StatCard
+            name="Completed Orders"
+            icon={CheckCircle}
+            value={orderStats.completedOrders}
+            color="#10B981"
+          />
+          <StatCard
+            name="Total Revenue"
+            icon={DollarSign}
+            value={orderStats.totalRevenue}
+            color="#EF4444"
+          />
+        </motion.div>
+
+        <section className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
+          <DailyOrders />
+          <OrderDistribution />
+        </section>
+
+        <OrdersTable />
+      </main>
+    </div>
+  );
+};
+export default OrdersPage;
